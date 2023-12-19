184f56e52d4f83596babe6b8079a3cb9a1766242
Edder Alberto López Villastrigo <lovedder1995@outlook.com>
2023-12-18 21:46:24 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+# github:lovedder1995/array-reduce#1.0.0
+
+return function ($array, $reducer) {
+    $index = -1;
+    return array_reduce($array, function($reduced, $item) use ($array, $reducer, &$index) {
+        $index++;
+        return $reducer($reduced, $item, $index, $array);
+    }, $array[0]);
+};
